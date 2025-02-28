42b0d69bcda589ef073fb93a33e272ce0abdb4ea
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2025-02-28 10:57:22 -0500

    modified enigma_sim

---
+++ enigma_sim/code/enigma_sim.py
@@ -0,0 +1,22 @@
+'''
+this code simulates the engigma machine and its series of encryptions. the code will 
+visualize the text as each encryption/decryption takes place. just like the real machine, 
+you put in either plain text or cypher text and if the machine is set correctly your 
+message will be encoded or decoded just like it was back in WWII. 
+
+EXAMPLE USAGE:
+# build env
+python3 -m venv myenv_enigma
+source myenv_enigma/bin/activate 
+pip install
+
+# run
+python3 ./enigma.py "<input_text>" "<plugboard_pairs>" "<reflector>" "<rotor_positions>" 
+			"<rotor1_#>" "<rotor2_#>" "<rotor3_#>" "<rotor4_#>" "<encrypt_decrypt_speed>"
+'''
+
+# imports
+
+# functions
+
+# run machine
