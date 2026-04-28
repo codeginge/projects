602797e5f416754d9ed1b9222eaf3162e3718a7b
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2026-04-28 10:58:27 -0400

    update

--- robotics/code/2_arm_drawing_robot/2_arm_drawing/2_arm_drawing.ino
+++ robotics/code/2_arm_drawing_robot/2_arm_drawing/2_arm_drawing.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+include <math.h>
 struct AnglePair {
   int theta_1; // angle of first linkage
   int theta_2; // angle of second linkage
@@ -74,8 +75,8 @@ AnglePair inverse_kinematics(float x, float y, float l1, float l2) {
   // pythonic: theta_2 = (math.acos((x*x+y*y-link_1*link_1-link_2*link_2)/(2*link_1*link_2)) - offset)*elbow_coefficient
   // python: theta_1 = math.atan2(y, x) - math.atan2((link_2*math.sin(theta_2)),(link_1+link_2*math.cos(theta_2)))
   AnglePair resultant_angles;
-  resultant_angles.theta_2 = acos((x*x+y*y-l1*l1-l2*l2)/(2*l1*l2))*180/PI;
-  resultant_angles.theta_1 = (atan(y/x)+atan((l2*sin(resultant_angles.theta_2))/(l1+l2*cos(resultant_angles.theta_2))))*180/PI;
+  resultant_angles.theta_2 = acos( (x*x + y*y - l1*l1 - l2*l2) / (2*l1*l2) ) * 180 / PI;
+  resultant_angles.theta_1 = (atan2(y/x)+atan2((l2*sin(resultant_angles.theta_2))/(l1+l2*cos(resultant_angles.theta_2))))*180/PI;
   return(resultant_angles);
 }
 
