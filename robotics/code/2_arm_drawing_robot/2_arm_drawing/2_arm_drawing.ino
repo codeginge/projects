64c14a24d304b43741a85afd0d64f417df3110f5
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2026-04-29 12:02:23 -0400

    update

--- robotics/code/2_arm_drawing_robot/2_arm_drawing/2_arm_drawing.ino
+++ robotics/code/2_arm_drawing_robot/2_arm_drawing/2_arm_drawing.ino
@@ -50,6 +50,7 @@ void loop() {
   if (program_type == 1) {
     if (Serial.available() > 0){
       // read serial for x, y coordinates and pen-down variable "x_float, y_float, pen_down" line by line
+      if (x_value == 0 && y_value == 0) {continue;}
       float x_value = Serial.parseFloat();
       float y_value = Serial.parseFloat();
       float pen_down = Serial.parseInt();
