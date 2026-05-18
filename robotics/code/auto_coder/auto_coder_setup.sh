1075f9df8ad89239ec2afddf38349182cca8b8be
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2026-05-18 11:10:19 -0400

    added setup script for auto coder

---
+++ robotics/code/auto_coder/auto_coder_setup.sh
@@ -0,0 +1,30 @@
+# exit if any error exists
+set -e
+echo "==============================================="
+echo "Running Setup Script for AUTO CODER for rpi 500"
+echo "==============================================="
+
+echo "--> Updating system packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "--> Installing python3 pip and dependencies..."
+sudo apt install python3-pip python3-venv git curl -y
+
+echo "--> Setting up python virtual environment..."
+python3 -m venv myenv
+source myenv/bin/activate
+
+echo "--> Installing Ollama and OpenCV libraries..."
+pip install --upgrade pip
+pip install ollama opencv-python
+
+echo "--> Installing Ollama system service..."
+curl -fsSL https://ollama.com | sh
+
+echo "--> Waiting for Ollama service to start..."
+sleep 5
+
+echo "--> Pulling Ollama Qwen2.5-VL 3B model"
+ollama pull qwen2.5vl:3b
+
+
