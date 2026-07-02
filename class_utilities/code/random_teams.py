25e3db2d2b468361c7737c5f7d02c17c6437b003
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2026-07-02 08:44:53 -0400

    added template for python script

---
+++ class_utilities/code/random_teams.py
@@ -0,0 +1,7 @@
+'''
+random_teams.py 
+by: Michael Roberts
+last updated: 07/02/2026
+'''
+
+
---
+++ templates/code/python_script.py
@@ -0,0 +1,28 @@
+'''
+code: 
+by: Michael Roberts
+last updated: //
+
+## code description:
+
+Build python environment:
+python3 -m venv myenv
+source myenv/bin/activate
+pip install ... 
+
+## example script call:
+python3 ./script.py \
+    --var1 20
+
+'''
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--var1", type=int, required=True, help="variable description")
+
+if __name__ = "__main__":
+    args = parse_args()
+    var1 = args.var1
+
