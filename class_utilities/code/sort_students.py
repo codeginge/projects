8b04bcca63f05660c0d3b67d00c7d1de97931223
Michael Roberts <michael@Michaels-MacBook-Air-2.local>
2026-09-11 10:07:49 -0400

    updated sorting and bin reference

--- class_utilities/code/sort_students.py
+++ class_utilities/code/sort_students.py
@@ -23,13 +23,24 @@ python3 ./sort_students.py --sort_order "0,2" --file "/directory/file.csv"
 
 import argparse
 
-def student_sort(sort_order, file):
-    with open(file, "r") as current_file:
+def create_bins(args):
+    [letters, rows, columns, exclude_bins] = args.bin_info.split(';')
+    bin_list = []
+    for l in letters:
+        for r in range(int(rows)):
+            for c in range(int(columns)):
+                bin_id = f"{l}-{r+1}-{c+1}"
+                if bin_id not in exclude_bins:
+                    bin_list.append(bin_id)
+    return(bin_list)
+
+def student_sort(args):
+    with open(args.file, "r") as current_file:
         students = current_file.read().splitlines()
         headers = students[0]
         students = students[1:]
         print(headers)
-        for col in sort_order.split(','):
+        for col in args.sort_order.split(','):
             students.sort(key = lambda x: x.split(',')[int(col)].strip())
         return(students) 
 
@@ -37,10 +48,12 @@ def parse_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--sort_order", type=str, help="rows to sort and the priority order")
     parser.add_argument("--file", type=str, help="file with student roster")
+    parser.add_argument("--bin_info", type=str, help="letters, rows, columns, bins to exclude ex: IJKL,5,5,['k-1-1','k-1-2','k-1-3','k-1-4','k-1-5']")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    students = student_sort(args.sort_order, args.file)
-    for s in students:
-        print(s)
+    students = student_sort(args)
+    bins = create_bins(args)
+    for index, s in enumerate(students):
+        print(f"{s} ------ {bins[index]}")
